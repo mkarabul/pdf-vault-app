08b58b4713a7f1e6807188fd01be4515bdd30104
Mert Karabulut <mkarabul@purdue.edu>
2024-08-16 12:04:49 +0300

    Finish implementing navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,8 +60,8 @@ export default function Home() {
                   <Image
                     src="/bled.jpg"
                     alt="product preview"
-                    width={1364}
-                    height={866}
+                    width={1400}
+                    height={1000}
                     quality={100}
                     className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
                   ></Image>
@@ -85,6 +85,69 @@ export default function Home() {
             ></div>
           </div>
         </div>
+      </div>
+
+      {/* {features section} */}
+      <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-40">
+        <div className="mb-12 px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl sm:text-center">
+            <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">
+              Start studying with flashcards now
+            </h2>
+            <p className="mt-4 text-lg text-gray-600">
+              Creating flashcards has never been easier. With PDF-Vault, you can
+              create flashcards from your PDFs in seconds.
+            </p>
+          </div>
+        </div>
+
+        {/* Steps */}
+        <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0 md:pl-4 md:pr-4">
+          <li className="md:flex-1">
+            <div
+              className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 
+            md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4"
+            >
+              <span className="text-sm font-medium text-blue-600">Step 1:</span>
+              <span className="text-xl font-semibold">
+                Sign up for an account
+              </span>
+              <span className="mt-2 text-zinc-700">
+                PDF-Vault is currently available at no cost. Sign up now to
+                begin.
+              </span>
+            </div>
+          </li>
+          <li className="md:flex-1">
+            <div
+              className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 
+            md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4"
+            >
+              <span className="text-sm font-medium text-blue-600">Step 2:</span>
+              <span className="text-xl font-semibold">
+                Upload a PDF to your account
+              </span>
+              <span className="mt-2 text-zinc-700">
+                Upload a PDF to your account to start creating flashcards.
+              </span>
+            </div>
+          </li>
+          <li className="md:flex-1">
+            <div
+              className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 
+            md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4"
+            >
+              <span className="text-sm font-medium text-blue-600">Step 3:</span>
+              <span className="text-xl font-semibold">
+                Higlight text to create flashcards
+              </span>
+              <span className="mt-2 text-zinc-700">
+                Highlight the text you want to include in your flashcards to
+                create them.
+              </span>
+            </div>
+          </li>
+        </ol>
       </div>
     </>
   );
